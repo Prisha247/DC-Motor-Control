b2e57563b71b59cc51973a37c27c89bf568cce44
Prisha Sadhwani <prishas0724@gmail.com>
2021-10-07 11:15:11 -0400

    working movement code

---
+++ working_movement_code/working_movement_code.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(3);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(4);
+
+void setup()  {
+  Serial.begin(9600);
+  AFMS.begin();
+
+}
+
+void loop() {
+  leftMotor->setSpeed(75);
+  rightMotor->setSpeed(75);
+
+  leftMotor->run(BACKWARD);
+  rightMotor->run(BACKWARD);
+
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+}
